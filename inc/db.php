c9e686c8cde36a126130000374213de28488a4e4
lo1cgsan <lo1cgsan@gmail.com>
2019-11-12 08:47:44 +0100

    początek aplikacji, baza danych

---
+++ inc/db.php
@@ -0,0 +1,12 @@
+<?php
+
+function init_baza() {
+	global $db, $dbfile, $kom;
+	if (!file_exists($dbfile))
+		$kom[] = 'Brak pliku bazy. Tworzę nowy.';
+
+	$db = new PDO("sqlite:$dbfile");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+?>
