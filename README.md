7986d31eef06108baf9825a2903c5669d9bff538
lo1cgsan <lo1cgsan@gmail.com>
2019-11-15 08:00:06 +0100

    tworzenie tabel w bazie

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bazaphp
+
+github.com/lo1cgsan/bazaphp
+
+## Linux
+
+Katalog "bazaphp" powinien znaleźć się w katalogu "public_html".
+Katalog "bazaphp/baza" powinien mięć ustawione prawa dostępu 777.
+
+### Uruchomienie aplikacji
+
+Adres w przeglądarce: localhost/~nazwa_uzytkownika/bazaphp
--- inc/db.php
+++ inc/db.php
@@ -9,4 +9,35 @@ function init_baza() {
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 
+function init_tables() {
+	global $db, $kom;
+	if (file_exists('baza/baza.sql')) {
+		$q = "SELECT name FROM sqlite_master WHERE type='table' AND name='menu'";
+		$ret = array();
+		db_query($q, $ret);
+		if (empty($ret)) {
+			$sql = file_get_contents('baza/baza.sql');
+			$db->exec($sql);
+			$kom[] = "Utworzono tabele!";
+		}
+	}
+}
+
+function db_query($q, &$ret) {
+	global $db;
+	$r = null;
+	try {
+		$r = $db->query($q);
+	} catch(PDOException $e) {
+		echo ($e->getMessage());
+	}
+	if ($r) {
+		$ret = $r->fetchAll();
+		return true;
+	}
+	return false;
+}
+
+
+
 ?>
